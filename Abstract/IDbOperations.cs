91e83817b903426782232b35b146e21b13d5bdc2
kadirmuratbaseren <kadirmuratbaseren@gmail.com>
2016-08-15 09:01:04 +0300

    initialize..

---
+++ Abstract/IDbOperations.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdoNetHelper.Abstract
+{
+    public interface IDbOperations<T, K>
+    {
+        int Insert();
+        int Update();
+        int Delete();
+        List<T> SelectAll();
+        T SelectById(K id);
+    }
+}
---
+++ Database.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdoNetHelper
+{
+    public class Database
+    {
+        public string ConnectionString { get; private set; }
+
+
+        public Database(string _connectionString)
+        {
+            ConnectionString = _connectionString;
+        }
+
+
+        public int RunQuery(string query, params SqlParameter[] parameters)
+        {
+            SqlConnection con = new SqlConnection(ConnectionString);
+            SqlCommand cmd = new SqlCommand(query, con);
+
+            if (parameters != null && parameters.Length > 0)
+            {
+                cmd.Parameters.AddRange(parameters.ToArray());
+            }
+
+            int result = 0;
+
+            con.Open();
+            result = cmd.ExecuteNonQuery();
+            con.Close();
+
+            return result;
+        }
+
+        public DataTable RunProc(string procName, params SqlParameter[] parameters)
+        {
+            SqlConnection con = new SqlConnection(ConnectionString);
+            SqlCommand cmd = new SqlCommand(procName, con);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            if (parameters != null && parameters.Length > 0)
+            {
+                cmd.Parameters.AddRange(parameters.ToArray());
+            }
+
+            DataTable dt = new DataTable();
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            adapter.Fill(dt);
+
+            return dt;
+        }
+
+        public DataTable GetTable(string query, params SqlParameter[] parameters)
+        {
+            SqlConnection con = new SqlConnection(ConnectionString);
+            SqlCommand cmd = new SqlCommand(query, con);
+
+            if (parameters != null && parameters.Length > 0)
+            {
+                cmd.Parameters.AddRange(parameters.ToArray());
+            }
+
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+
+            // Adaptor : otomatik bağlantı açar. Verileri çeker(sorguyu çalıştırır) ve bir datatable 'a doldurur ve bağlantıyı otomatik kapatır.
+
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+            return dt;
+        }
+
+    }
+}
