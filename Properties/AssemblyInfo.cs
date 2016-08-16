ca4aa2927697e6eb9f31924928a364c6e871063c
kadirmuratbaseren <kadirmuratbaseren@gmail.com>
2016-08-16 09:58:05 +0300

    Update Assembly and Assembly File Version Number

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,13 +5,13 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AdoNetHelper")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("KMB ADO.NET Helper")]
+[assembly: AssemblyDescription("K. Murat Başeren | www.muratbaseren.com.tr")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AdoNetHelper")]
+[assembly: AssemblyCompany("K. Murat Başeren")]
+[assembly: AssemblyProduct("KMB ADO.NET Helper")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTrademark("K. Murat Başeren")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.1.*")]
+
